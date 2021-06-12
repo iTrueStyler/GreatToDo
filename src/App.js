0c0108bde52dc5ae7776a902591addae966b6354
iTrueStyler <rjcvfnbh100@gmail.com>
2021-06-12 13:35:08 +0300

    continue after pause

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import List from './components/List/index';
-
+import AddList from './components/AddList/index.jsx'
 
 function App() {
+  
+
   return (
     <div className='todo'>
       <div className='todo__sidebar'> 
@@ -31,18 +33,7 @@ function App() {
             }
           ]}
           />
-          <List  items = {[
-            {className:'list__add-button',
-              icon:<svg width="12" height="12" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg" 
-              >
-              <path d="M8 1V15" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-              <path d="M1 8H15" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-              </svg>
-              ,
-              name:'Добавить список'
-            }
-          ]}
-          />
+          <AddList/>
           
           
       </div>
--- src/components/List/index.jsx
+++ src/components/List/index.jsx
@@ -4,18 +4,18 @@ import classNames from 'classnames';
 
 function List({ items }) {
   return (
-    <div>
-      <ul className="list">
-        {items.map((item,index) => (
-          <li key={index} className={classNames(item.classNames,{'active':item.active})}>
-            <i>
-              {item.icon?item.icon:<i className={`badge badge--${item.color}`}></i>}
-            </i>
-            <span>{item.name}</span>
-          </li>
-        ))}
-      </ul>
-    </div>
+
+    <ul className="list">
+      {items.map((item, index) => (
+        <li key={index} className={classNames(item.className, { 'active': item.active })}>
+          <i>
+            {item.icon ? item.icon : <i className={`badge badge--${item.color}`}></i>}
+          </i>
+          <span>{item.name}</span>
+        </li>
+      ))}
+    </ul>
+
   );
 }
 
